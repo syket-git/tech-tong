ce8ac86b45cbe6556958f45b94ed21557d6078a8
syket <syketweb@gmail.com>
2023-12-10 11:20:49 +0600

    socket server initialized

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,8 +1,16 @@
 import http from "http";
+import SocketService from "./services/socket";
 
 async function init() {
   const httpServer = http.createServer();
+  // initialized the socket service;
+  const socketService = new SocketService();
+
   const PORT = process.env.PORT ? process.env.PORT : 8000;
+
+  //attaching our http server to the socket server
+  socketService.io.attach(httpServer);
+
   httpServer.listen(PORT, () =>
     console.log(`HTTP server started at port ${PORT}`)
   );
